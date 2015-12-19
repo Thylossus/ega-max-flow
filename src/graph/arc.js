8d50163d73c196b6c527605ab4b33eff96fa4eec
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2015-12-19 16:21:05 +0100

    added arc class and test for it
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

---
+++ src/graph/arc.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+  
+  var count = 0;
+
+  function id_gen() {
+    count += 1;
+    return `a${count}`;
+  }
+
+  class Arc {
+    constructor(from, to, capacity) {
+      this.id = id_gen();
+      this.from = from;
+      this.to = to;
+      this.capacity = capacity;
+    }
+  }
+
+  exports.create = (from, to, capacity) => {
+    return new Arc(from, to, capacity);
+  }
+
+})();
