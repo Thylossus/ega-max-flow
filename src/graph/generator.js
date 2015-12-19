8bd89d9872658138ad917d2f8ce18d7ffec340f1
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2015-12-19 17:43:39 +0100

    added run method to the generator
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

--- src/graph/generator.js
+++ src/graph/generator.js
@@ -1,3 +1,10 @@
+const range = require('lodash/utility/range');
+const random = require('lodash/number/random');
+const flatten = require('lodash/array/flatten');
+const vertex = require('./vertex');
+const arc = require('./arc');
+const graph = require('./graph');
+
 (function () {
   'use strict';
 
@@ -5,6 +12,21 @@
     constructor(numberOfVertices, maxCapacity) {
       this.numberOfVertices = numberOfVertices;
       this.maxCapacity = maxCapacity;
+    }
+
+    run() {
+      let vertices = range(this.numberOfVertices).map(() => {
+        return vertex.create();
+      });
+
+      let arcs = vertices.map((v1) => {
+          return vertices.map((v2) => {
+            return arc.create(v1, v2, random(this.maxCapacity));
+          });
+        });
+      arcs = flatten(arcs);
+
+      return graph.create(vertices, arcs);
     }
   }
 
--- test/graph/generator.js
+++ test/graph/generator.js
@@ -11,15 +11,15 @@ describe('Generator', () => {
     describe('#create', () => {
 
       it('should instatiate', () => {
-        let vertices = 10;
+        let numberOfVertices = 10;
         let maxCapacity = 100;
 
-        let g = generator.create(vertices, maxCapacity);
+        let g = generator.create(numberOfVertices, maxCapacity);
 
         expect(g).to.be.an('object');
         expect(g).to.have.property('numberOfVertices').that.is.a('number');
         expect(g).to.have.property('maxCapacity').that.is.a('number');
-        expect(g.numberOfVertices).to.equal(vertices);
+        expect(g.numberOfVertices).to.equal(numberOfVertices);
         expect(g.maxCapacity).to.equal(maxCapacity);
 
       });
@@ -28,7 +28,28 @@ describe('Generator', () => {
   });
 
   describe('class methods', () => {
+    describe('#run', () => {
 
+      it('should return a graph', () => {
+        let numberOfVertices = 10;
+        let maxCapacity = 100;
+
+        let gen = generator.create(numberOfVertices, maxCapacity);
+        let graph = gen.run();
+
+        expect(graph).to.be.an('object');
+        expect(graph.vertices).to.have.length(numberOfVertices);
+        // The minimum number of edges in a connected graph is at least n - 1
+        expect(graph.arcs.length).to.be.above(numberOfVertices - 2);
+
+        let noArcExceedsMaxCapacity = graph.arcs.every((arc) => {
+          return arc.capacity <= maxCapacity;
+        });
+
+        expect(noArcExceedsMaxCapacity).to.be.true;
+      });
+
+    });
   });
 
 
